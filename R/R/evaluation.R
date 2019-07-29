e4dfc572af01890ca71885641fb36efeb2b5a3ab
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-07-30 00:40:09 +0200

    Added training lines.

--- R/R/evaluation.R
+++ R/R/evaluation.R
@@ -11,5 +11,13 @@ evaluation <- function(individual, train_mode, I, O) {
     i <- i + 1
   }
   model <- keras::keras_model_sequential()
-  model %>% layer_dense(units = hidden_layers[1], input_shape = c(I))
+  model %>% keras::layer_dense(units = hidden_layers[1], input_shape = c(I),
+                        activation = "relu")
+  for (layer in tail(hidden_layers, 1)) {
+    model %>% keras::layer_dense(units = layer, activation = "relu")
+  }
+  model %>% keras::layer_dense(units = O, activation = "softmax")
+  model %>% keras::compile(optimizer = "adam", loss = "categorical_crossentropy",
+                           metrics = c("accuracy"))
+
 }
