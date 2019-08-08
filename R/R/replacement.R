6139704948dd0377a677645c61f76e70b096fd90
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-08-08 12:52:08 +0200

    Remove old package definition.

--- R/R/replacement.R
+++
@@ -1,4 +0,0 @@
-replacement <- function(population, population.size) {
-  ordered.population <- population[order(unlist(population$loss)), ]
-  return(ordered.population[c(1:population.size), ])
-}
--- R/R/selection.R
+++
@@ -1,9 +0,0 @@
-selection <- function(population, child.number) {
-  matting.pool <- data.frame()
-  for (j in c(1:(child.number / 2))) {
-    parents <- population[sample(nrow(population), child.number / 2), ]
-    ordered.parents <- parents[order(unlist(parents$loss)), ]
-    matting.pool <- rbind(matting.pool, head(ordered.parents, 2))
-  }
-  return(matting.pool)
-}
