860b5887b81e316d281c0c815cbd805f8b97d033
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2019-08-12 11:45:44 +0200

    Updated test.

--- tests/testthat/test-get_best_nn.R
+++ tests/testthat/test-get_best_nn.R
@@ -3,5 +3,5 @@ context("Testing NNEvol")
 test_that("Testing function get_best_nn returns a correct value", {
   data <- data.frame(X1 = c(1:10), X2 = c(11:20), X3 = c(1:10), Y1 = c(0:9), stringsAsFactors = FALSE)
   res <- get_best_nn(data, 3, 1, train_mode = 1, population_size = 5, generation_number = 2, seed = 123)
-  expect_equal(res$architecture, "nnn/n/n")
+  expect_equal(class(res$architecture), "character")
 })
